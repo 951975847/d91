ce3eb4d817dd226b61962135077ab47b5699b85c
951975847 <951975847@qq.com>
2020-08-27 09:51:41 +0800

    第一次提交

---
+++ src/main/java/com/java/hahaha/HahahahaApplication.java
@@ -0,0 +1,13 @@
+package com.java.hahaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahahahaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HahahahaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/hahaha/HahahahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.hahaha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahahahaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
