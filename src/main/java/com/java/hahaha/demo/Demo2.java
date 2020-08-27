5adae79134031dc27ca2d6ef59cbb7be55684c73
951975847 <951975847@qq.com>
2020-08-27 10:07:58 +0800

    第2次提交

---
+++ src/main/java/com/java/hahaha/demo/Demo2.java
@@ -0,0 +1,12 @@
+package com.java.hahaha.demo;
+
+/**
+ * @author ZYC
+ * @Description
+ * @create 2020-08-10:06
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________");
+    }
+}
